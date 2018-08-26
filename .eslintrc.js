b293ef52a345e29022a238d0835735ea3e45407e
Olov Johansson <johansson.olov@gmail.com>
2018-08-26 18:59:32 +0200

    Added lint and randomness

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,17 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    "plugin:vue/essential",
+    "plugin:prettier/recommended",
+    "eslint:recommended"
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ["vue"],
   // add your custom rules here
-  rules: {}
-}
+  rules: { "no-console": 0 }
+};
